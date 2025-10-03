adf8e68e9ebb0362e9f0199ff2dd7cf465431780
aiandyou50 <me@aiandyou.me>
2025-10-04 01:46:52 +0900

    Update index.js

--- src/index.js
+++ src/index.js
@@ -1,16 +1,4 @@
-// This is the entry point for your Worker.
-// This file is intentionally blank for a static site deployment.
-// The `wrangler.toml` file will automatically configure the Worker
-// to serve static assets from the directory specified in the `[site].bucket` property.
-
-// For more details, refer to the Cloudflare Workers documentation for static sites:
-// https://developers.cloudflare.com/workers/platform/sites/
-
-export default {
-  async fetch(request, env, ctx) {
-    // This worker is a passthrough for static assets.
-    // It doesn't need to do anything special.
-    // The static assets are served automatically by the configuration in wrangler.toml
-    return new Response("This is a passthrough worker for a static site. The content should be served from the 'public' directory.");
-  },
-};
+// This worker is used to enable static site serving via the `[site]` configuration in wrangler.toml.
+// By existing, it signals to Wrangler that this is a Worker project.
+// It does not need an active fetch handler, as the static assets will be served automatically.
+export default {};
